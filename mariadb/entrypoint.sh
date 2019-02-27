c2ae7c23d9d364d7e3486b8515d506554ddecd0b
black4t <myugan59@gmail.com>
2019-02-27 23:14:14 +0700

    fixing

--- mariadb/entrypoint.sh
+++ mariadb/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo service mysql start
-mysql -uroot -p -e "CREATE DATABASE IF NOT EXISTS $MARIADB_NAME;"
-mysql -uroot -p -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MARIADB_ROOT_PASS'"
-mysql -uroot -p -e "GRANT ALL PRIVILEGES ON "$MARIADB_NAME".* TO '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASS'"
-mysql -uroot -p -e "FLUSH PRIVILEGES;"
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $MARIADB_NAME;"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MARIADB_ROOT_PASS'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON "$MARIADB_NAME".* TO '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASS'"
+mysql -uroot -e "FLUSH PRIVILEGES;"
 while true; do sleep 1d; done 
