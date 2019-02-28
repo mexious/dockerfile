39134f201319619ef5ad3ca0618dbd36936e5f9f
black4t <myugan59@gmail.com>
2019-02-28 11:23:19 +0700

    add sed function

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 sudo service nginx start
+sed -i "s/localhost/$FQDN/g" /etc/nginx/sites-enabled/default.conf
+sed -i "s/webroot/$WEBROOT/g" /etc/nginx/sites-enabled/default.conf
+sed -i "s/php-fpm/$FPM/g" /etc/nginx/sites-enabled/default.conf
 while true; do sleep 1d; done
 exec "$@"
